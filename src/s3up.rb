057c3c6e40d1f21388a21349ed78db3a96cfd235
Chad Neal <chad.neal@jeppesen.com>
2013-11-03 18:27:37 -0700

    Multi file upload, directory test
    WIll upload more than 1 file at a time.
    Will test if the upload is a directory and refuse to upload if so.

--- src/s3up.rb
+++ src/s3up.rb
@@ -6,12 +6,19 @@ require 'yaml'
 require 'terminal-notifier'
 
 AWS.config(YAML.load_file(File.expand_path("~/.aws/awsconfig.yaml")))
-s3 = AWS::S3.new
+$s3 = AWS::S3.new
 
-bucket_name = 'www.chadneal.com'
-file_name = ARGV[0]
-key_name = File.basename(file_name)
-url = 'http://' << bucket_name << '/' << key_name
+def upload(file_name)
+  bucket_name = 'www.chadneal.com'
+  key_name = File.basename(file_name)
+  url = 'http://' << bucket_name << '/' << key_name
+  
+  bucket = $s3.buckets[bucket_name]
+  bucket.objects[key_name].write(:file => file_name)
+  
+  pbcopy(url)
+  TerminalNotifier.notify("File is now available @ " << url)
+end
 
 def pbcopy(input)
   str = input.to_s
@@ -23,8 +30,11 @@ def pbpaste
   `pbpaste`
 end
 
-bucket = s3.buckets[bucket_name]
-obj = bucket.objects[key_name].write(:file => file_name)
-
-pbcopy(url)
-TerminalNotifier.notify("File is now available @ " << url)
+ARGV.each do |f|
+  puts f
+  if File.file?(f)
+    upload(f)
+  else
+    TerminalNotifier.notify("s3up will not upload directory " << f)
+  end
+end
